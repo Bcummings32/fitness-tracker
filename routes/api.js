9175c5a23aedec7452f9613a5534110caedb2055
Ben Cummings <bncmmngs@aol.com>
2021-01-11 14:15:48 -0500

    dashboard changes

--- routes/api.js
+++ routes/api.js
@@ -10,12 +10,20 @@ router.post("/api/workouts", (req, res) => {
     })
 })
 router.get("/api/workouts", (req, res) => {
-    Workout.find().then(seeWorkouts => {
-        res.json(seeWorkouts)
+    Workout.find().then(dbWorkouts => {
+        res.json(dbWorkouts)
     }).catch(err => {
         res.json(err)
     })
 })
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7).then(dbWorkouts => {
+        res.json(dbWorkouts)
+    }).catch(err => {
+        res.json(err)
+    })
+})
+
 //update
 router.put("/api/workouts/:id", ({ body, params}, res) => {
     Workout.findByIdAndUpdate(params.id, {$push: {exercises:body}}, {new:true, 
