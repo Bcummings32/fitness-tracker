dfde474f1ca46f5fc08780b0e6d1439434dd7c5d
Ben Cummings <bncmmngs@aol.com>
2021-01-12 18:07:43 -0500

    atlas deployment

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-// routes
+
+//connect to remote host
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+mongoose.connect(MONGODB_URI);
+
+// mongoose.connect("mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+// // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 app.listen(PORT, () => {
